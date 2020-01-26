66612fdd9590df4d9f6a543433c44d98617868fb
iamisafi <isafi@gmail.com>
2020-01-26 16:17:37 +0600

    simple react app with redux

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Hello World</p>
+        </div>
+    )
+}
+
+export default Contact;
